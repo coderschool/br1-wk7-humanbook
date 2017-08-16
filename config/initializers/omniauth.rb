aa2c7f8ab2e3edf02075cbe8dd19edb599cf84b8
Harley Trung <harley@coderschool.vn>
2017-08-16 15:13:00 +0700

    use ENV vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '122937648345181', '1859d305b90ec8cb3ce45c14587b3c11'
+  provider :facebook, ENV["FB_APP_ID"] || '122937648345181', ENV["FB_APP_SECRET"] || '1859d305b90ec8cb3ce45c14587b3c11'
 end
