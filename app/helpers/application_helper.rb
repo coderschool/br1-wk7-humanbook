abef29011f94af78aab61e5622331f0e47679600
Harley Trung <harley@coderschool.vn>
2017-08-17 08:48:42 +0700

    theming

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
   def class_for flash_type
-    { success: 'is-primary', error: 'is-danger', notice: 'is-warning'}[flash_type.to_sym]
+    { success: 'is-success', error: 'is-danger', notice: 'is-warning'}[flash_type.to_sym]
   end
 
   def flash_messages(opts = {})
     flash.map do |msg_type, message|
-      content_tag(:div, class: "notification #{class_for(msg_type)}") do
-        (message + content_tag(:button, 'x'.html_safe, class: 'delete')).html_safe
+      content_tag(:div, class: "message #{class_for(msg_type)}") do
+        content_tag(:div, class: "message-header") do
+          (message + content_tag(:button, 'x'.html_safe, class: 'delete')).html_safe
+        end
       end
     end.join.html_safe
   end
