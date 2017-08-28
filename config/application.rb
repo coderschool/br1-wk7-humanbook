1e318b32cf36a94334ce0210e80a7670dc576741
Harley Trung <harley@coderschool.vn>
2017-08-28 07:01:38 +0700

    update ENV variables for redis and sendgrid smtp

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,13 @@ module Humanbook
     unless Rails.env.test?
       config.action_mailer.delivery_method = :smtp
       ActionMailer::Base.smtp_settings = {
-        :port           => ENV['MAILGUN_SMTP_PORT'],
-        :address        => ENV['MAILGUN_SMTP_SERVER'],
-        :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-        :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-        :domain         => 'yourapp.heroku.com',
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :domain => ENV['APP_DOMAIN'] || 'humanbook.herokuapp.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
         :authentication => :plain,
+        :enable_starttls_auto => true
       }
     end
   end
